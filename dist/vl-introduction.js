a33abeb716f41fb21b2c1e1ed88acd3f63de8436
Tom Coemans <tom.coemans@gmail.com>
2020-04-16 16:02:58 +0200

    dist

---
+++ dist/vl-introduction.js
@@ -0,0 +1,21 @@
+import { NativeVlElement, define } from '/node_modules/vl-ui-core/dist/vl-core.js';
+
+/**
+ * VlIntroduction
+ * @class
+ * @classdesc Gebruik deze component als introductie van de website. Deze component krijgt een opvallende layout zodat de gebruiker zijn aandacht getrokken wordt.
+ * 
+ * @extends NativeVlElement
+ * 
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-introduction/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-introduction/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-introduction.html|Demo}
+ * 
+ */
+export class VlIntroduction extends NativeVlElement(HTMLParagraphElement) {
+    connectedCallback() {
+        this.classList.add('vl-introduction');
+    }
+}
+
+define('vl-introduction', VlIntroduction, { extends: 'p' });
