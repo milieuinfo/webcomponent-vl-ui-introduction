b80fa07eecd55a59e0cf9a51e23eac611aad5569
Tom Coemans <tom.coemans@gmail.com>
2020-06-03 18:17:50 +0200

    fixed test

--- test/e2e/components/vl-introduction.js
+++ test/e2e/components/vl-introduction.js
@@ -1,5 +1,10 @@
 const {VlElement} = require('vl-ui-core').Test;
 
-class VlIntroduction extends VlElement {}
+class VlIntroduction extends VlElement {
+  async getText() {
+    const text = await super.getText();
+    return text.trim();
+  }
+}
 
 module.exports = VlIntroduction;
