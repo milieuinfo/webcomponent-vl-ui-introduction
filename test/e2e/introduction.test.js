7bca79a718cc3183af7ffec4aabedceb7f5b1289
coemans <tom.coemans@zeuzios.be>
2021-03-22 15:19:52 +0100

    update dependencies and added WCAG e2e test (#28)

--- test/e2e/introduction.test.js
+++ test/e2e/introduction.test.js
@@ -9,6 +9,10 @@ describe('vl-introduction', async () => {
     return vlIntroductionPage.load();
   });
 
+  it('WCAG', async () => {
+    await assert.eventually.isFalse(vlIntroductionPage.hasWcagIssues());
+  });
+
   it('als gebruiker kan in de introductie lezen', async () => {
     const introduction = await vlIntroductionPage.getIntroduction();
     await assert.eventually.equal(introduction.getText(), 'Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Sed posuere consectetur est at lobortis. Etiam porta sem malesuada magna mollis euismod. Vivamus sagittis lacus vel augue laoreet rutrum faucibus.');
